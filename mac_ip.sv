3efc361e2efa171a12c4b5125453a9a2416d4f46
ShaShuShi <58037426+ShaShuShi@users.noreply.github.com>
2025-01-28 16:39:01 -0600

    Add files via upload

--- mac_ip.sv
+++ mac_ip.sv
@@ -12,14 +12,15 @@ module mac_ip(
     typedef enum reg {IDLE, ACC} state_t;
     state_t state, nxt_state;
 
-    reg [23:0] CoutReg;
     logic [15:0] ab;
+    logic [15:0] ab_flopped;
     logic acc;
     logic [23:0] dataa;
     logic [23:0] datab;
     logic [23:0] result;
 
     lpm_mult_ip iMULT(.dataa(Ain), .datab(Bin), .result(ab));
+    lpm_add_sub_ip iADD(.dataa(dataa), .datab(datab), .result(result));
 
     always_ff @(posedge clk, negedge rst_n) begin
         if (!rst_n) begin
@@ -52,21 +53,68 @@ module mac_ip(
 
     always_ff @(posedge clk, negedge rst_n) begin
 	if (!rst_n) begin
-	    CoutReg <= '0;
-	    dataa <= '0;
-	    datab <= '0;
+	   ab_flopped <= '0;
 	end else if (Clr) begin
-	    CoutReg <= '0;
-	    dataa <= '0;
-	    datab <= '0;
-	end else if (acc) begin
-	    dataa <= CoutReg;
-	    datab <= ab;
-	    CoutReg <= result;
+	   ab_flopped <= '0;
+	end else begin
+	   ab_flopped <= ab;
 	end
     end
 
-    lpm_add_sub_ip iADD(.dataa(dataa), .datab(datab), .result(result));
+    always_ff @(posedge clk, negedge rst_n) begin
+	if (!rst_n) begin
+	    dataa <= '0;
+	    datab <= '0;
+	end else if (Clr) begin
+	    dataa <= '0;
+	    datab <= '0;
+	end else if (acc) begin
+	    dataa <= result;
+	    if (ab_flopped == ab) begin
+		datab <= 0;
+	    end else begin
+		datab <= ab;
+	    end
+	end
+    end
+
+    
     assign Cout = result;
 
 endmodule
+
+/*
+module mac_ip(
+    input clk,
+    input rst_n,
+    input En,
+    input Clr,
+    input [7:0] Ain,
+    input [7:0] Bin,
+    output [23:0] Cout
+);
+
+    logic [15:0] ab;
+    logic [23:0] datab;
+    logic [23:0] result;
+
+    lpm_mult_ip iMULT(.dataa(Ain), .datab(Bin), .result(ab));
+
+    lpm_add_sub_ip iADD(.dataa(ab), .datab(datab), .result(result));
+    
+    always_ff @(posedge clk, negedge rst_n) begin
+
+     if(!rst_n) begin
+         datab = 0;
+     end else if(Clr) begin
+         datab = 0;
+     end else if(En) begin
+	 datab = result; 
+     end
+
+    end
+
+    assign Cout = datab;
+
+endmodule
+*/
